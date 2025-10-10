9b435f5a4fb8a6ecabda28548d8155415f775d0f
George Ongoro <gtechong72@gmail.com>
2025-10-10 16:22:12 +0300

    done

--- assets/js/hero-carousel.js
+++ assets/js/hero-carousel.js
@@ -6,18 +6,17 @@
   const imageFolder = new URL("../images/gallery/sports-day/", scriptSrc).href;
   const images = [
     "1738753971198.jpg",
-    "DSC_0357.jpg",
-    "DSC_0358.jpg",
-    "DSC_0359.jpg",
-    "DSC_0360.jpg",
-    "DSC_0377.jpg",
-    "DSC_0413_1.jpg",
-    "DSC_0419_1.jpg",
-    "DSC_0454.jpg",
-    "DSC_0500.jpg",
-    "DSC_0517.jpg",
+    "sc1.jpg",
+    "sc2.jpg",
+    "sc3.jpg",
+    "sc4.jpg",
+    "sc5.jpg",
+    "sc6.jpg",
+    "sc7.jpg",
+    "sc8.jpg",
+    "sc9.jpg",
+    "sc10.jpg",
     "img1.jpg",
-    "img6.jpg",
   ];
 
   const slideInterval = 4000; // ms
